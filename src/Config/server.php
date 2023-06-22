b4a1832a54ceb981d293fbe799740cf3244365d4
rafaeltovar <hola@rafaeltovar.info>
2023-06-22 12:57:48 +0200

    clean

--- src/Config/server.php
+++ src/Config/server.php
@@ -4,7 +4,6 @@ return [
     's3' => [
         'AWS_S3_BUCKET' => 'default',
         'AWS_S3_PREFIX' => 'prefix/',
-        // 'AWS_S3_PUT_REQUEST_OPTIONS' => [],
 
         'AWS_S3_CACHE_PREFIX' => 'prefix/cache/',
         'AWS_S3_CACHE_PUT_REQUEST_OPTIONS' => [],
--- src/Http/Response.php
+++ src/Http/Response.php
@@ -8,7 +8,6 @@ extends TusResponse
 {
     public function __construct()
     {
-        // parent::__construct();
         $this->createOnly(true);
     }
 }
